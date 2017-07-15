747ee3de18e276ceb926fa4a2127016a3abe11fa
151610171-smkassalaambandung <151610223@smkassalaambandung.sch.id>
2017-07-15 14:30:10 +0700

    sudah selesai

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::resource('/coba', 'CobaaController');
 Route::group(['prefix'=>'admin','middleware'=>['auth','role:admin']], function(){
 	Route::resource('authors','AuthorsController');
 	Route::resource('books','BooksController');
+	Route::resource('members', 'MembersController');
+	Route::get('statistics', [
+		'as' => 'statistics.index',
+		'uses' => 'StatisticsController@index'
+		]);
 });
 
 Route::get('books/{book}/borrow', [
